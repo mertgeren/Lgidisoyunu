38be27fc5146d4a9b25aded0a58bcbc678c38573
mertgeren <38167309+mertgeren@users.noreply.github.com>
2018-04-08 02:38:17 +0300

    Add files via upload

---
+++ Odev2_152804010_Mert_Geren/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Odev2_152804010_Mert_Geren
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblZaman = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.radioButon9 = new System.Windows.Forms.RadioButton();
+            this.radioButon8 = new System.Windows.Forms.RadioButton();
+            this.radioButon7 = new System.Windows.Forms.RadioButton();
+            this.radioButon6 = new System.Windows.Forms.RadioButton();
+            this.radioButon5 = new System.Windows.Forms.RadioButton();
+            this.dataGridViewOyunAlani = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOyunAlani)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 310);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Skor :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Oyun Süresi :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(30, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 17);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Oyun Alanının Boyutunu Seçiniz :";
+            // 
+            // lblZaman
+            // 
+            this.lblZaman.AutoSize = true;
+            this.lblZaman.Location = new System.Drawing.Point(131, 275);
+            this.lblZaman.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblZaman.Name = "lblZaman";
+            this.lblZaman.Size = new System.Drawing.Size(0, 17);
+            this.lblZaman.TabIndex = 26;
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Location = new System.Drawing.Point(80, 310);
+            this.lblSkor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(0, 17);
+            this.lblSkor.TabIndex = 25;
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(33, 174);
+            this.btnBasla.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(214, 58);
+            this.btnBasla.TabIndex = 24;
+            this.btnBasla.Text = "BAŞLA";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // radioButon9
+            // 
+            this.radioButon9.AutoSize = true;
+            this.radioButon9.Location = new System.Drawing.Point(103, 133);
+            this.radioButon9.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButon9.Name = "radioButon9";
+            this.radioButon9.Size = new System.Drawing.Size(58, 21);
+            this.radioButon9.TabIndex = 19;
+            this.radioButon9.Text = "9 * 9";
+            this.radioButon9.UseVisualStyleBackColor = true;
+            // 
+            // radioButon8
+            // 
+            this.radioButon8.AutoSize = true;
+            this.radioButon8.Location = new System.Drawing.Point(167, 103);
+            this.radioButon8.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButon8.Name = "radioButon8";
+            this.radioButon8.Size = new System.Drawing.Size(58, 21);
+            this.radioButon8.TabIndex = 20;
+            this.radioButon8.Text = "8 * 8";
+            this.radioButon8.UseVisualStyleBackColor = true;
+            // 
+            // radioButon7
+            // 
+            this.radioButon7.AutoSize = true;
+            this.radioButon7.Location = new System.Drawing.Point(167, 74);
+            this.radioButon7.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButon7.Name = "radioButon7";
+            this.radioButon7.Size = new System.Drawing.Size(58, 21);
+            this.radioButon7.TabIndex = 21;
+            this.radioButon7.Text = "7 * 7";
+            this.radioButon7.UseVisualStyleBackColor = true;
+            // 
+            // radioButon6
+            // 
+            this.radioButon6.AutoSize = true;
+            this.radioButon6.Location = new System.Drawing.Point(43, 103);
+            this.radioButon6.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButon6.Name = "radioButon6";
+            this.radioButon6.Size = new System.Drawing.Size(58, 21);
+            this.radioButon6.TabIndex = 22;
+            this.radioButon6.Text = "6 * 6";
+            this.radioButon6.UseVisualStyleBackColor = true;
+            // 
+            // radioButon5
+            // 
+            this.radioButon5.AutoSize = true;
+            this.radioButon5.Location = new System.Drawing.Point(43, 74);
+            this.radioButon5.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButon5.Name = "radioButon5";
+            this.radioButon5.Size = new System.Drawing.Size(58, 21);
+            this.radioButon5.TabIndex = 23;
+            this.radioButon5.Text = "5 * 5";
+            this.radioButon5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewOyunAlani
+            // 
+            this.dataGridViewOyunAlani.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOyunAlani.Location = new System.Drawing.Point(315, 37);
+            this.dataGridViewOyunAlani.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridViewOyunAlani.Name = "dataGridViewOyunAlani";
+            this.dataGridViewOyunAlani.Size = new System.Drawing.Size(10, 10);
+            this.dataGridViewOyunAlani.TabIndex = 18;
+            this.dataGridViewOyunAlani.Click += new System.EventHandler(this.dataGridViewOyunAlani_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 479);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblZaman);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.radioButon9);
+            this.Controls.Add(this.radioButon8);
+            this.Controls.Add(this.radioButon7);
+            this.Controls.Add(this.radioButon6);
+            this.Controls.Add(this.radioButon5);
+            this.Controls.Add(this.dataGridViewOyunAlani);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOyunAlani)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblZaman;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.RadioButton radioButon9;
+        private System.Windows.Forms.RadioButton radioButon8;
+        private System.Windows.Forms.RadioButton radioButon7;
+        private System.Windows.Forms.RadioButton radioButon6;
+        private System.Windows.Forms.RadioButton radioButon5;
+        private System.Windows.Forms.DataGridView dataGridViewOyunAlani;
+    }
+}
+
---
+++ Odev2_152804010_Mert_Geren/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev2_152804010_Mert_Geren
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int oyunAlanBoyutu = 0;
+        public int count = 0;
+        DateTime baslangicZamani = new DateTime();
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+
+            count = 0;
+
+            if (radioButon5.Checked)
+            {
+                oyunAlanBoyutu = 5;
+            }
+            else if (radioButon6.Checked)
+            {
+                oyunAlanBoyutu = 6;
+            }  
+            else if (radioButon7.Checked)
+            {
+                oyunAlanBoyutu = 7;
+            }   
+            else if (radioButon8.Checked)
+            {
+                oyunAlanBoyutu = 8;
+            }     
+            else if (radioButon9.Checked)
+            {
+                oyunAlanBoyutu = 9;
+            }    
+            else
+            {
+                MessageBox.Show("Oyun Alanının Boyutunu Belirlemediniz !");
+            } 
+
+            dataGridViewOyunAlani.Enabled = true;
+            dataGridViewOyunAlani.Rows.Clear();
+            dataGridViewOyunAlani.Columns.Clear();
+            dataGridViewOyunAlani.Size = new Size(oyunAlanBoyutu * 50, oyunAlanBoyutu * 50);
+
+            for (int i = 0; i < oyunAlanBoyutu; i++)
+            {
+                dataGridViewOyunAlani.Columns.Add(null, null);
+                DataGridViewColumn Column = dataGridViewOyunAlani.Columns[i];
+                Column.Width = 40;
+            }
+            for (int j = 0; j < oyunAlanBoyutu - 1; j++)
+            {
+                dataGridViewOyunAlani.Rows.Add(null, null);
+                DataGridViewRow Row = dataGridViewOyunAlani.Rows[j];
+                Row.Height = 40;
+            }
+        }
+
+        private void dataGridViewOyunAlani_Click(object sender, EventArgs e)
+        {
+
+            int SelectedRow = dataGridViewOyunAlani.CurrentCell.RowIndex;
+            int SelectedColumn = dataGridViewOyunAlani.CurrentCell.ColumnIndex;
+
+            if (dataGridViewOyunAlani.Rows[SelectedRow].Cells[SelectedColumn].Style.BackColor == Color.LightBlue || count == 0)
+            {
+                if (count == 0)
+                {
+                    baslangicZamani = DateTime.Now;
+                }
+                    
+                lblZaman.Text = (DateTime.Now - baslangicZamani).ToString();
+
+                for (int i = 0; i < oyunAlanBoyutu; i++)
+                {
+                    for (int j = 0; j < oyunAlanBoyutu; j++)
+                    {
+                        if (dataGridViewOyunAlani.Rows[i].Cells[j].Style.BackColor != Color.Pink)
+                        {
+                            dataGridViewOyunAlani.Rows[i].Cells[j].Style.BackColor = Color.White;
+                        }
+                    }
+                }
+
+                count++;
+
+                dataGridViewOyunAlani.Rows[dataGridViewOyunAlani.CurrentCell.RowIndex].Cells[dataGridViewOyunAlani.CurrentCell.ColumnIndex].Value = count;
+                dataGridViewOyunAlani.Rows[dataGridViewOyunAlani.CurrentCell.RowIndex].Cells[dataGridViewOyunAlani.CurrentCell.ColumnIndex].Style.BackColor = Color.Pink;
+
+                if (SelectedRow + 2 < oyunAlanBoyutu && SelectedColumn + 1 < oyunAlanBoyutu && dataGridViewOyunAlani.Rows[SelectedRow + 2].Cells[SelectedColumn + 1].Style.BackColor != Color.Pink)
+                {
+                    dataGridViewOyunAlani.Rows[SelectedRow + 2].Cells[SelectedColumn + 1].Style.BackColor = Color.LightBlue;
+                }
+                if (SelectedRow + 2 < oyunAlanBoyutu && 0 <= SelectedColumn - 1 && dataGridViewOyunAlani.Rows[SelectedRow + 2].Cells[SelectedColumn - 1].Style.BackColor != Color.Pink)
+                {
+                    dataGridViewOyunAlani.Rows[SelectedRow + 2].Cells[SelectedColumn - 1].Style.BackColor = Color.LightBlue;
+                }    
+                if (SelectedRow - 2 >= 0 && SelectedColumn + 1 < oyunAlanBoyutu && dataGridViewOyunAlani.Rows[SelectedRow - 2].Cells[SelectedColumn + 1].Style.BackColor != Color.Pink)
+                {
+                    dataGridViewOyunAlani.Rows[SelectedRow - 2].Cells[SelectedColumn + 1].Style.BackColor = Color.LightBlue;
+                }   
+                if (SelectedRow - 2 >= 0 && SelectedColumn - 1 >= 0 && dataGridViewOyunAlani.Rows[SelectedRow - 2].Cells[SelectedColumn - 1].Style.BackColor != Color.Pink)
+                {
+                    dataGridViewOyunAlani.Rows[SelectedRow - 2].Cells[SelectedColumn - 1].Style.BackColor = Color.LightBlue;
+                }   
+                if (SelectedRow + 1 < oyunAlanBoyutu && SelectedColumn + 2 < oyunAlanBoyutu && dataGridViewOyunAlani.Rows[SelectedRow + 1].Cells[SelectedColumn + 2].Style.BackColor != Color.Pink)
+                {
+                    dataGridViewOyunAlani.Rows[SelectedRow + 1].Cells[SelectedColumn + 2].Style.BackColor = Color.LightBlue;
+                }   
+                if (SelectedRow + 1 < oyunAlanBoyutu && SelectedColumn - 2 >= 0 && dataGridViewOyunAlani.Rows[SelectedRow + 1].Cells[SelectedColumn - 2].Style.BackColor != Color.Pink)
+                {
+                    dataGridViewOyunAlani.Rows[SelectedRow + 1].Cells[SelectedColumn - 2].Style.BackColor = Color.LightBlue;
+                }   
+                if (SelectedRow - 1 >= 0 && SelectedColumn + 2 < oyunAlanBoyutu && dataGridViewOyunAlani.Rows[SelectedRow - 1].Cells[SelectedColumn + 2].Style.BackColor != Color.Pink)
+                {
+                    dataGridViewOyunAlani.Rows[SelectedRow - 1].Cells[SelectedColumn + 2].Style.BackColor = Color.LightBlue;
+                } 
+                if (SelectedRow - 1 >= 0 && SelectedColumn - 2 >= 0 && dataGridViewOyunAlani.Rows[SelectedRow - 1].Cells[SelectedColumn - 2].Style.BackColor != Color.Pink)
+                {
+                    dataGridViewOyunAlani.Rows[SelectedRow - 1].Cells[SelectedColumn - 2].Style.BackColor = Color.LightBlue;
+                }    
+            }
+
+            if (dataGridViewOyunAlani.Rows[SelectedRow].Cells[SelectedColumn].Style.BackColor == Color.LightBlue)
+            {
+                dataGridViewOyunAlani.Rows[SelectedRow].Cells[SelectedColumn].Value = count;
+                dataGridViewOyunAlani.Rows[SelectedRow].Cells[SelectedColumn].Style.BackColor = Color.Pink;
+            }
+
+            lblSkor.Text = count.ToString();
+
+            if (count == oyunAlanBoyutu * oyunAlanBoyutu)
+            {
+                MessageBox.Show("TEBRİKLER KAZANDINIZ !");
+            }
+        }
+    }
+}
